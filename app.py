137974a15655af2431d785031640f9f20b0182aa
ymdtky28 <ymdtky28@gmail.com>
2026-02-04 21:51:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,5 @@ st.set_page_config(page_title="トヨタ野球成績", layout="wide")
 st.title("⚾ トヨタ自動車 野球打撃成績")
 
 # あなたがアップロードした実際のファイル名に修正しました
-# 修正前
-target = '打撃成績表_トヨタ全打者vs全投手.xlsx - Sheet1.csv'
-
-# 修正後（フォルダ名の "date/" を最初に追加します）
-target = 'date/打撃成績表_トヨタ全打者vs全投手.xlsx - Sheet1.csv'
+# ファイルの指定
+target = 'data.csv'
