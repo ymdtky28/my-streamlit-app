160c0fa277dba8208fc70e10e8b4f441c2498d06
ymdtky28 <ymdtky28@gmail.com>
2026-02-04 21:04:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-import os
-import streamlit as st
+# データを読み込む
+df_2025 = load_baseball_stats('data/成績表.xlsx - 2025.csv')
 
-# 1. そもそも data フォルダがあるか？
-if os.path.exists('data'):
-    files = os.listdir('data')
-    st.write("dataフォルダの中にあるファイルはこれです：")
-    st.code(files) # ここに表示された名前をそのままコピーして使う
-else:
-    st.error("data フォルダ自体がリポジトリに存在しません！")
-
-# 2. 今の場所（カレントディレクトリ）にある全てのファイル
-st.write("すべてのファイル構成:")
-for root, dirs, files in os.walk("."):
-    for file in files:
-        st.write(os.path.join(root, file))
+# 【重要】これを書かないと画面には何も出ません
+if df_2025 is not None:
+    st.write("### 2025年 成績表")
+    st.dataframe(df_2025)
